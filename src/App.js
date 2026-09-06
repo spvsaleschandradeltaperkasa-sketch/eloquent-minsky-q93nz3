8b88673b5357bb69a61cfaa5b26c15124e38c21b
spvsaleschandradeltaperkasa-sketch <spvsaleschandradeltaperkasa@gmail.com>
2026-09-06 22:14:52 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ export default function App() {
   const [statusFilter, setStatusFilter] = useState('ALL');
   const [jenisFilter, setJenisFilter] = useState('ALL');
 
-  const publishedCsvUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTo9EJbez7MyWXlYKXc-rzon8VqYa1SEYc_ffeObmb0Nq9D6hTAzdS1rbZ6_OnnYtvAYYoTIMQu03C/pub?gid=586995800&single=true&output=csv";
+  const publishedCsvUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTo9EJbez7MyWXlYkXc-rzoN8vqYa1SEyC_ffeObmb0Nq9D6hTAzdS1rbZ6_OnnYntvAYYoTIMQu03C/pub?output=csv;
 
   const parseCSVLine = (line) => {
     const result = [];
